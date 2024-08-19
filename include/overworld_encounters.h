d8510e0a0127e2bbf15052fa0b41dd375b9ea948
HashtagMarky <samuel.mark01@outlook.com>
2024-08-19 22:46:00 +0100

    Move GetOverworldMonSpecies to New File

--- include/overworld_encounters.h
+++ include/overworld_encounters.h
@@ -1,6 +1,7 @@
 #ifndef GUARD_OVERWORLD_ENCOUNTERS_H
 #define GUARD_OVERWORLD_ENCOUNTERS_H
 
+void GetOverworldMonSpecies(void);
 void GetOverworldSpeciesCatchRate(void);
 
 #endif // GUARD_OVERWORLD_ENCOUNTERS_H
