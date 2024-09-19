8261e8ecaaa2733f6939080b8af5f21bb2391217
HashtagMarky <samuel.mark01@outlook.com>
2024-09-19 11:07:40 +0100

    Convert Ikigai Preset Names to Compound String Array

--- src/main_menu.c
+++ src/main_menu.c
@@ -510,6 +510,11 @@ static const u8 *const sFemalePresetNames[] = {
     gText_DefaultNameHalie
 };
 
+static const u8 *const sIkigaiPresetNames[] = {
+    COMPOUND_STRING("KOLE"),
+    COMPOUND_STRING("ANKA")
+};
+
 // The number of male vs. female names is assumed to be the same.
 // If they aren't, the smaller of the two sizes will be used and any extra names will be ignored.
 #define NUM_PRESET_NAMES min(ARRAY_COUNT(sMalePresetNames), ARRAY_COUNT(sFemalePresetNames))
@@ -2198,18 +2203,12 @@ void NewGameSamuelSpeech_SetDefaultPlayerName(u8 nameId)
     gSaveBlock2Ptr->playerName[PLAYER_NAME_LENGTH] = EOS;
 }
 
-const u8 gText_Kole[] = _("KOLE");
-const u8 gText_Anka[] = _("ANKA");
-
 static void NewGameSamuelSpeech_SetPlayerNameKoleAnka(void)
 {
     const u8 *name;
     u8 i;
 
-    if (gSaveBlock2Ptr->playerGender == MALE)
-        name = gText_Kole;
-    else
-        name = gText_Anka;
+    name = sIkigaiPresetNames[gSaveBlock2Ptr->playerGender];
     for (i = 0; i < PLAYER_NAME_LENGTH; i++)
         gSaveBlock2Ptr->playerName[i] = name[i];
     gSaveBlock2Ptr->playerName[PLAYER_NAME_LENGTH] = EOS;
