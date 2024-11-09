9c70cda472f495b7327947a6d1555c6d35f0be96
HashtagMarky <samuel.mark01@outlook.com>
2024-11-09 19:03:21 +0000

    Change Options Text Colours To Coloured Selection

--- src/option_plus_menu.c
+++ src/option_plus_menu.c
@@ -711,11 +711,11 @@ static void DrawRightSideChoiceText(const u8 *text, int x, int y, bool8 choosen,
     if (active)
     {
         color_red[0]    = TEXT_COLOR_TRANSPARENT;
-        color_red[1]    = TEXT_COLOR_OPTIONS_ORANGE_FG;
-        color_red[2]    = TEXT_COLOR_OPTIONS_GRAY_FG;
+        color_red[1]    = TEXT_COLOR_OPTIONS_WHITE;
+        color_red[2]    = TEXT_COLOR_OPTIONS_GRAY_LIGHT_FG;
         color_gray[0]   = TEXT_COLOR_TRANSPARENT;
-        color_gray[1]   = TEXT_COLOR_OPTIONS_WHITE;
-        color_gray[2]   = TEXT_COLOR_OPTIONS_GRAY_LIGHT_FG;
+        color_gray[1]   = TEXT_COLOR_OPTIONS_ORANGE_FG;
+        color_gray[2]   = TEXT_COLOR_OPTIONS_GRAY_FG;
     }
     else
     {
@@ -723,15 +723,15 @@ static void DrawRightSideChoiceText(const u8 *text, int x, int y, bool8 choosen,
         color_red[1]    = TEXT_COLOR_OPTIONS_ORANGE_FG;
         color_red[2]    = TEXT_COLOR_OPTIONS_GRAY_FG;
         color_gray[0]   = TEXT_COLOR_TRANSPARENT;
-        color_gray[1]   = TEXT_COLOR_OPTIONS_ORANGE_FG;
-        color_gray[2]   = TEXT_COLOR_OPTIONS_GRAY_FG;
+        color_gray[1]   = TEXT_COLOR_OPTIONS_WHITE;
+        color_gray[2]   = TEXT_COLOR_OPTIONS_GRAY_LIGHT_FG;
     }
 
 
     if (choosen)
     {
         AddTextPrinterParameterized4(WIN_OPTIONS, FONT_NORMAL, x, y, 0, 0, color_gray, TEXT_SKIP_DRAW, text);
-        if (!active)
+        if (active)
         {
             LoadPalette(&selectedTextColor, OPTIONS_TEXT_OFFSET + color_gray[1], sizeof(selectedTextColor));
             LoadPalette(&selectedShadowColor, OPTIONS_TEXT_OFFSET + color_gray[2], sizeof(selectedShadowColor));
@@ -740,7 +740,7 @@ static void DrawRightSideChoiceText(const u8 *text, int x, int y, bool8 choosen,
     else
     {
         AddTextPrinterParameterized4(WIN_OPTIONS, FONT_NORMAL, x, y, 0, 0, color_red, TEXT_SKIP_DRAW, text);
-        if (active)
+        if (!active)
         {
             LoadPalette(&selectedTextColor, OPTIONS_TEXT_OFFSET + color_red[1], sizeof(selectedTextColor));
             LoadPalette(&selectedShadowColor, OPTIONS_TEXT_OFFSET + color_red[2], sizeof(selectedShadowColor));
