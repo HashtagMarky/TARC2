fc3887627b6a293cb1abfc7236ac67aff795124e
HashtagMarky <samuel.mark01@outlook.com>
2024-11-12 16:57:36 +0000

    Update sound.c

--- src/sound.c
+++ src/sound.c
@@ -503,6 +503,8 @@ void PlayCryInternal(u16 species, s8 pan, s8 volume, u8 priority, u8 mode)
     case CRY_MODE_WEAK:
         pitch = 15000;
         break;
+    case CRY_MODE_RHH_INTRO:
+        break;
     }
 
     SetPokemonCryVolume(volume);
