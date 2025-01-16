341fac032fb9b1735a5427f0cba731494c18f3a6
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-01-16 23:05:26 +0000

    Refactor Skippable Cutscene Code

--- include/cutscene.h
+++ include/cutscene.h
@@ -6,6 +6,6 @@ struct CutscenesSkips
     const u8 *scriptCutsceneSkips;
 };
 
-void DestroyCutsceneTask(void);
+void EndSkippableCutscene(void);
 
 #endif // GUARD_CUTSCENE_H
--- src/cutscene.c
+++ src/cutscene.c
@@ -8,107 +8,104 @@
 #include "sprite.h"
 #include "task.h"
 
-extern const u8 PetalburgCity_EventScript_CutsceneOne[];
+#define TAG_CUTSCENE_SKIP_BUTTON 5550
 
-#define SPRITE_TAG_SKIP_BUTTON 5550
+static const u16 sCutsceneSkipButton_Gfx[] = INCBIN_U16("graphics/cutscene/skip_button.4bpp");
+static const u16 sCutsceneSkipButton_Pal[] = INCBIN_U16("graphics/cutscene/skip_button.gbapal");
 
-static const u16 sSkipButton_Gfx[] = INCBIN_U16("graphics/cutscene/skip_button.4bpp");
-static const u16 sSkipButton_Pal[] = INCBIN_U16("graphics/cutscene/skip_button.gbapal");
-
-static const struct SpriteSheet sSkipButtonSpriteSheets[] = {
-    {(const void *)sSkipButton_Gfx, sizeof(sSkipButton_Gfx), SPRITE_TAG_SKIP_BUTTON},
+static const struct SpriteSheet sCutsceneSkipButtonSpriteSheets[] = {
+    {(const void *)sCutsceneSkipButton_Gfx, sizeof(sCutsceneSkipButton_Gfx), TAG_CUTSCENE_SKIP_BUTTON},
     {0}
 };
 
-static const union AnimCmd sSkipButtonAnim[] = {
+static const union AnimCmd sCutsceneSkipButtonAnim[] = {
     ANIMCMD_FRAME(0, 16),
     ANIMCMD_FRAME(8, 16),
     ANIMCMD_JUMP(0)
 };
 
-static const union AnimCmd *const sSkipButtonAnimTable[] = {
-    sSkipButtonAnim
+static const union AnimCmd *const sCutsceneSkipButtonAnimTable[] = {
+    sCutsceneSkipButtonAnim
 };
 
-static const struct SpritePalette sSkipButtonPalette = {sSkipButton_Pal, SPRITE_TAG_SKIP_BUTTON};
+static const struct SpritePalette sCutsceneSkipButtonPalette = {sCutsceneSkipButton_Pal, TAG_CUTSCENE_SKIP_BUTTON};
 
-static const struct OamData sSkipButtonOamData = {
+static const struct OamData sCutsceneSkipButtonOamData = {
     .shape = ST_OAM_H_RECTANGLE,
     .size = 2
 };
 
-static const struct SpriteTemplate sSkipButtonTemplate = {
-    SPRITE_TAG_SKIP_BUTTON,
-    SPRITE_TAG_SKIP_BUTTON,
-    &sSkipButtonOamData,
-    sSkipButtonAnimTable,
+static const struct SpriteTemplate sCutsceneSkipButtonTemplate = {
+    TAG_CUTSCENE_SKIP_BUTTON,
+    TAG_CUTSCENE_SKIP_BUTTON,
+    &sCutsceneSkipButtonOamData,
+    sCutsceneSkipButtonAnimTable,
     NULL,
     gDummySpriteAffineAnimTable,
     SpriteCallbackDummy
 };
 
+static void Task_SkippableCutscene(u8 taskId);
+static bool32 TaskHelper_SkippableCutscene_CheckCutsceneFlag(u32 cutscene);
+static void TaskHelper_SkippableCutscene_SetCutsceneFlag(u32 cutscene);
+
 
 #define tCutscene data[0]
 #define tSpriteId data[1]
-static void Task_CutsceneTask(u8 taskId)
+void StartSkippableCutscene(struct ScriptContext *ctx)
+{
+    u32 cutscene = ScriptReadByte(ctx);
+
+    if (TaskHelper_SkippableCutscene_CheckCutsceneFlag(cutscene))
+    {
+        u8 taskId;
+        u8 spriteId;
+
+        taskId = CreateTask(Task_SkippableCutscene, 64);
+        gTasks[taskId].tCutscene = cutscene;
+
+        LoadSpriteSheets(sCutsceneSkipButtonSpriteSheets);
+        LoadSpritePalette(&sCutsceneSkipButtonPalette);
+        spriteId = CreateSprite(&sCutsceneSkipButtonTemplate, 20, 50, 0xFF);
+        gSprites[spriteId].oam.priority = 0;
+        gTasks[taskId].tSpriteId = spriteId;
+    }
+    else
+    {
+        TaskHelper_SkippableCutscene_SetCutsceneFlag(cutscene);
+    }
+}
+
+static void Task_SkippableCutscene(u8 taskId)
 {
     u32 cutscene = gTasks[taskId].tCutscene;
     if (JOY_NEW(START_BUTTON))
     {
         ScriptContext_SetupScript(sCutsceneSkipScripts[cutscene].scriptCutsceneSkips);
-        DestroyCutsceneTask();
+        EndSkippableCutscene();
     }
 }
 
-bool32 CheckCutsceneFlag(u32 cutscene)
+void EndSkippableCutscene(void)
+{
+    u8 taskId = FindTaskIdByFunc(Task_SkippableCutscene);
+    if (taskId != TASK_NONE)
+    {
+        DestroySprite(&gSprites[gTasks[taskId].tSpriteId]);
+        FreeSpriteTilesByTag(TAG_CUTSCENE_SKIP_BUTTON);
+        FreeSpritePaletteByTag(TAG_CUTSCENE_SKIP_BUTTON);
+        DestroyTask(taskId);
+    }
+}
+
+static bool32 TaskHelper_SkippableCutscene_CheckCutsceneFlag(u32 cutscene)
 {
     return TRUE;
 }
 
-void SetCutsceneFlag(u32 cutscene)
+static void TaskHelper_SkippableCutscene_SetCutsceneFlag(u32 cutscene)
 {
     
 }
-
-void CreateCutsceneTask(struct ScriptContext *ctx)
-{
-    u32 cutscene = ScriptReadByte(ctx);
-
-    if (CheckCutsceneFlag(cutscene))
-    {
-        u8 taskId = CreateTask(Task_CutsceneTask, 64);
-        gTasks[taskId].tCutscene = cutscene;
-        LoadSpriteSheets(sSkipButtonSpriteSheets);
-        LoadSpritePalette(&sSkipButtonPalette);
-
-        u8 spriteId = CreateSprite(&sSkipButtonTemplate, 20, 50, 0xFF);
-        gTasks[taskId].tSpriteId = spriteId;
-        gSprites[spriteId].oam.priority = 0;
-    }
-    else
-    {
-        SetCutsceneFlag(cutscene);
-    }
-}
-
-void DestroyCutsceneTask(void)
-{
-    u8 taskId = FindTaskIdByFunc(Task_CutsceneTask);
-    if (taskId != TASK_NONE)
-    {
-        DestroySprite(&gSprites[gTasks[taskId].tSpriteId]);
-        FreeSpriteTilesByTag(SPRITE_TAG_SKIP_BUTTON);
-        FreeSpritePaletteByTag(SPRITE_TAG_SKIP_BUTTON);
-        DestroyTask(taskId);
-    }
-}
 #undef tCutscene
 #undef tSpriteId
-
-void CheckCutsceneTask(void)
-{
-    if (FindTaskIdByFunc(Task_CutsceneTask) != TASK_NONE)
-        DebugPrintf("Active");
-    else
-        DebugPrintf("Inactive");
-}
