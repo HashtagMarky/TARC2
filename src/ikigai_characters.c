a4ba165b9eaf6d7966fed9f69e33b41e4c8653dd
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-01-22 11:19:30 +0000

    Disable CHARACTER_NAME_TEXT_COLOUR

--- src/ikigai_characters.c
+++ src/ikigai_characters.c
@@ -761,7 +761,7 @@ static uq4_12_t GetGymTypeEffectiveness(u16 species, bool32 speciesAtk)
     modifier[0] = uq4_12_multiply(modifier[1], modifier[2]);
     return modifier[0];
 }
-
+#define CHARACTER_NAME_TEXT_COLOUR FALSE
 u8 IkigaiCharacter_ReturnMessageBoxPersonalityPalette(u32 character)
 {
     u32 textColour = 2;
@@ -788,7 +788,10 @@ u8 IkigaiCharacter_ReturnMessageBoxPersonalityPalette(u32 character)
     if (gSpeakerName == gIkigaiCharactersInfo[CHARACTER_DEFAULT].name)
         textColour = 2;
 
-    return textColour;
+    if (CHARACTER_NAME_TEXT_COLOUR)
+        return textColour;
+    else
+        return 2;
 }
 
 u8 CreateDialogueOptionIconSprite(u32 dialogueIndex)
