21b312caedb1783f0b47c45ba28a5e2e3ed9eacf
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-04-11 18:18:55 +0100

    Ikigai Interface Proper Filepaths

--- src/ikigai_interface.c
+++ src/ikigai_interface.c
@@ -7,55 +7,55 @@
 #include "palette.h"
 
 
-static const u32 IkigaiScrollingBgTiles[]               = INCBIN_U32("graphics/ikigai_scrolling_background/scroll_tiles.4bpp.lz");
+static const u32 IkigaiScrollingBgTiles[]               = INCBIN_U32("graphics/ikigai_interface/scroll_tiles.4bpp.lz");
 
-static const u32 IkigaiScrollingBgTilemap_PalZero[]     = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/zero.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalOne[]      = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/one.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalTwo[]      = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/two.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalThree[]    = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/three.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalFour[]     = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/four.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalFive[]     = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/five.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalSix[]      = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/six.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalSeven[]    = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/seven.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalEight[]    = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/eight.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalNine[]     = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/nine.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalTen[]      = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/ten.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalEleven[]   = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/eleven.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalTwelve[]   = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/twelve.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalThirteen[] = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/thirteen.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalFourteen[] = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/fourteen.bin.lz");
-static const u32 IkigaiScrollingBgTilemap_PalFifteen[]  = INCBIN_U32("graphics/ikigai_scrolling_background/tilemaps/fifteen.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalZero[]     = INCBIN_U32("graphics/ikigai_interface/tilemaps/zero.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalOne[]      = INCBIN_U32("graphics/ikigai_interface/tilemaps/one.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalTwo[]      = INCBIN_U32("graphics/ikigai_interface/tilemaps/two.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalThree[]    = INCBIN_U32("graphics/ikigai_interface/tilemaps/three.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalFour[]     = INCBIN_U32("graphics/ikigai_interface/tilemaps/four.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalFive[]     = INCBIN_U32("graphics/ikigai_interface/tilemaps/five.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalSix[]      = INCBIN_U32("graphics/ikigai_interface/tilemaps/six.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalSeven[]    = INCBIN_U32("graphics/ikigai_interface/tilemaps/seven.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalEight[]    = INCBIN_U32("graphics/ikigai_interface/tilemaps/eight.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalNine[]     = INCBIN_U32("graphics/ikigai_interface/tilemaps/nine.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalTen[]      = INCBIN_U32("graphics/ikigai_interface/tilemaps/ten.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalEleven[]   = INCBIN_U32("graphics/ikigai_interface/tilemaps/eleven.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalTwelve[]   = INCBIN_U32("graphics/ikigai_interface/tilemaps/twelve.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalThirteen[] = INCBIN_U32("graphics/ikigai_interface/tilemaps/thirteen.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalFourteen[] = INCBIN_U32("graphics/ikigai_interface/tilemaps/fourteen.bin.lz");
+static const u32 IkigaiScrollingBgTilemap_PalFifteen[]  = INCBIN_U32("graphics/ikigai_interface/tilemaps/fifteen.bin.lz");
 
-static const u16 IkigaiScrollingBgPal_Default[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/default.gbapal");
-static const u16 IkigaiScrollingBgPal_Tornadus[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/tornadus.gbapal");
-static const u16 IkigaiScrollingBgPal_Thundurus[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/thundurus.gbapal");
-static const u16 IkigaiScrollingBgPal_Landorus[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/landorus.gbapal");
-static const u16 IkigaiScrollingBgPal_Enamorus[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/enamorus.gbapal");
+static const u16 IkigaiScrollingBgPal_Default[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/default.gbapal");
+static const u16 IkigaiScrollingBgPal_Tornadus[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/tornadus.gbapal");
+static const u16 IkigaiScrollingBgPal_Thundurus[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/thundurus.gbapal");
+static const u16 IkigaiScrollingBgPal_Landorus[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/landorus.gbapal");
+static const u16 IkigaiScrollingBgPal_Enamorus[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/enamorus.gbapal");
 
-static const u16 IkigaiScrollingBgPal_Normal[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/normal.gbapal");
-static const u16 IkigaiScrollingBgPal_Fighting[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/fighting.gbapal");
-static const u16 IkigaiScrollingBgPal_Flying[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/flying.gbapal");
-static const u16 IkigaiScrollingBgPal_Poison[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/poison.gbapal");
-static const u16 IkigaiScrollingBgPal_Ground[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/ground.gbapal");
-static const u16 IkigaiScrollingBgPal_Rock[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/rock.gbapal");
-static const u16 IkigaiScrollingBgPal_Bug[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/bug.gbapal");
-static const u16 IkigaiScrollingBgPal_Ghost[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/ghost.gbapal");
-static const u16 IkigaiScrollingBgPal_Steel[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/steel.gbapal");
-static const u16 IkigaiScrollingBgPal_Fire[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/fire.gbapal");
-static const u16 IkigaiScrollingBgPal_Water[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/water.gbapal");
-static const u16 IkigaiScrollingBgPal_Grass[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/grass.gbapal");
-static const u16 IkigaiScrollingBgPal_Electric[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/electric.gbapal");
-static const u16 IkigaiScrollingBgPal_Psychic[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/psychic.gbapal");
-static const u16 IkigaiScrollingBgPal_Ice[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/ice.gbapal");
-static const u16 IkigaiScrollingBgPal_Dragon[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/dragon.gbapal");
-static const u16 IkigaiScrollingBgPal_Dark[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/dark.gbapal");
-static const u16 IkigaiScrollingBgPal_Fairy[] = INCBIN_U16("graphics/ikigai_scrolling_background/scroll_bg_palettes/fairy.gbapal");
+static const u16 IkigaiScrollingBgPal_Normal[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/normal.gbapal");
+static const u16 IkigaiScrollingBgPal_Fighting[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/fighting.gbapal");
+static const u16 IkigaiScrollingBgPal_Flying[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/flying.gbapal");
+static const u16 IkigaiScrollingBgPal_Poison[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/poison.gbapal");
+static const u16 IkigaiScrollingBgPal_Ground[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/ground.gbapal");
+static const u16 IkigaiScrollingBgPal_Rock[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/rock.gbapal");
+static const u16 IkigaiScrollingBgPal_Bug[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/bug.gbapal");
+static const u16 IkigaiScrollingBgPal_Ghost[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/ghost.gbapal");
+static const u16 IkigaiScrollingBgPal_Steel[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/steel.gbapal");
+static const u16 IkigaiScrollingBgPal_Fire[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/fire.gbapal");
+static const u16 IkigaiScrollingBgPal_Water[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/water.gbapal");
+static const u16 IkigaiScrollingBgPal_Grass[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/grass.gbapal");
+static const u16 IkigaiScrollingBgPal_Electric[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/electric.gbapal");
+static const u16 IkigaiScrollingBgPal_Psychic[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/psychic.gbapal");
+static const u16 IkigaiScrollingBgPal_Ice[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/ice.gbapal");
+static const u16 IkigaiScrollingBgPal_Dragon[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/dragon.gbapal");
+static const u16 IkigaiScrollingBgPal_Dark[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/dark.gbapal");
+static const u16 IkigaiScrollingBgPal_Fairy[] = INCBIN_U16("graphics/ikigai_interface/scroll_bg_palettes/fairy.gbapal");
 
-static const u16 IkigaiUIPal_Default[] = INCBIN_U16("graphics/ikigai_scrolling_background/ui_tiles_palettes/default.gbapal");
-static const u16 IkigaiUIPal_Tornadus[] = INCBIN_U16("graphics/ikigai_scrolling_background/ui_tiles_palettes/tornadus.gbapal");
-static const u16 IkigaiUIPal_Thundurus[] = INCBIN_U16("graphics/ikigai_scrolling_background/ui_tiles_palettes/thundurus.gbapal");
-static const u16 IkigaiUIPal_Landorus[] = INCBIN_U16("graphics/ikigai_scrolling_background/ui_tiles_palettes/landorus.gbapal");
-static const u16 IkigaiUIPal_Enamorus[] = INCBIN_U16("graphics/ikigai_scrolling_background/ui_tiles_palettes/enamorus.gbapal");
+static const u16 IkigaiUIPal_Default[] = INCBIN_U16("graphics/ikigai_interface/ui_tiles_palettes/default.gbapal");
+static const u16 IkigaiUIPal_Tornadus[] = INCBIN_U16("graphics/ikigai_interface/ui_tiles_palettes/tornadus.gbapal");
+static const u16 IkigaiUIPal_Thundurus[] = INCBIN_U16("graphics/ikigai_interface/ui_tiles_palettes/thundurus.gbapal");
+static const u16 IkigaiUIPal_Landorus[] = INCBIN_U16("graphics/ikigai_interface/ui_tiles_palettes/landorus.gbapal");
+static const u16 IkigaiUIPal_Enamorus[] = INCBIN_U16("graphics/ikigai_interface/ui_tiles_palettes/enamorus.gbapal");
 
 static const u32 *const sIkigaiScrollingBgTilemaps[16] =
 {
