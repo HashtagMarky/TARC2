4c0f79289951f7fba535024ca0ee5da2d8564d83
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-05-21 17:49:52 +0100

    Clean Up Saving Functions from Start Menu

--- src/start_menu.c
+++ src/start_menu.c
@@ -955,6 +955,11 @@ static void InitSave(void)
     sSavingComplete = FALSE;
 }
 
+void InitSave_Global(void)
+{
+    InitSave();
+}
+
 static u8 RunSaveCallback(void)
 {
     // True if text is still printing
@@ -965,6 +970,11 @@ static u8 RunSaveCallback(void)
 
     sSavingComplete = FALSE;
     return sSaveDialogCallback();
+}
+
+u8 RunSaveCallback_Global(void)
+{
+    return RunSaveCallback();
 }
 
 void SaveGame(void)
