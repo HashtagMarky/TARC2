c80622138db4b132248957d91174f4413c481e58
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-05-21 18:16:54 +0100

    Prevent Wrap Around Menu Scrolling

--- src/rotom_phone_start_menu.c
+++ src/rotom_phone_start_menu.c
@@ -1126,14 +1126,14 @@ static void RotomPhone_SmallStartMenu_HandleInput(bool32 down)
         }
     }
 
+    PlaySE(SE_SELECT);
     nextIndex = optionCurrent + offset;
     if (nextIndex >= ROTOM_PHONE_SMALL_OPTION_COUNT
         || nextIndex < ROTOM_PHONE_SMALL_OPTION_1
         || sRotomPhone_StartMenu->menuSmallOptions[nextIndex] == ROTOM_PHONE_MENU_COUNT)
-        nextIndex = ROTOM_PHONE_SMALL_OPTION_1;
+        return;
 
     menuSelected = sRotomPhone_StartMenu->menuSmallOptions[nextIndex];
-    PlaySE(SE_SELECT);
     RotomPhone_SmallStartMenu_UpdateMenuName();
 }
 
