a74f7e49989c2ef35fe7b03c377b9097cac59ec3
HashtagMarky <143505183+HashtagMarky@users.noreply.github.com>
2025-05-29 17:42:49 +0100

    Write Ikigai Character Descriptions Fit in PokéSphere Test

--- test/text.c
+++ test/text.c
@@ -826,3 +826,20 @@ TEST("Battle strings fit on the battle message window")
     Free(battleString);
 }
 //*/
+
+#include "ikigai_characters.h"
+#include "constants/ikigai_characters.h"
+TEST("Character Descriptions Fit in PokéSphere")
+{
+    u32 i;
+    const u32 fontId = FONT_SHORT_NARROWER, widthPx = 136 - 10;
+    u32 character = CHARACTER_FIRST;
+    for (i = CHARACTER_FIRST; i < CHARACTER_COUNT_TOTAL; i++)
+    {
+        if (gIkigaiCharactersInfo[i].highlights == NULL)
+            continue;
+        
+        PARAMETRIZE_LABEL("%S", gIkigaiCharactersInfo[i].highlights) { character = i; }
+    }
+    EXPECT_LE(GetStringWidth(fontId, gIkigaiCharactersInfo[character].highlights, 0), widthPx);
+}
